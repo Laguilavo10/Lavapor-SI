3c3ae7743a69c4069dd82db267ffa3418b02d665
Andres Laguilavo <lagui2003@gmail.com>
2023-02-09 08:52:21 -0500

    feat: create the router of the SI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { Menu } from "./components/Menu"
+import { HashRouter, Routes, Route } from "react-router-dom"
+import { UserProvider } from "./context/userData"
+import { Login } from "./pages/Login"
 import MainLayout from "./pages/MainLayout"
 
 //styles
-import './styles/Login.css'
-import './styles/MainLayout.css'
-import './styles/Menu.css'
+import "./styles/Login.css"
+import "./styles/MainLayout.css"
+import "./styles/Menu.css"
 
 function App() {
   return (
-    // <Login></Login>+
-    <MainLayout/>
+    <HashRouter>
+      <UserProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/main" element={<MainLayout />} />
+        </Routes>
+      </UserProvider>
+    </HashRouter>
   )
 }
 
