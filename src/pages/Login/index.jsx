bb5b7bd226f022215e707106ba8818e9ab08334a
Andres Laguilavo <lagui2003@gmail.com>
2023-02-09 08:53:19 -0500

    feat: add logic for the LogIn component

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,25 +1,59 @@
-import React from "react"
+import React, { useRef, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useUser } from "../../context/userData"
+import { users } from "../../utils/users"
 
 export function Login() {
+  const [viewPassword, setViewPassword] = useState(false)
+  const FormUser = useRef(null)
+  const navigate = useNavigate()
+  let { setUserData } = useUser()
+
+  const handleVisibilityPassword = () => {
+    setViewPassword((prevState) => !prevState)
+  }
+
+  const validateUser = (event) => {
+    event.preventDefault()
+    const formData = new FormData(FormUser.current)
+    const data = {
+      user: formData.get("user"),
+      password: formData.get("password"),
+    }
+    let canLogIn = users.some(
+      (u) => u.id === data.user && u.password === data.password
+    )
+
+    if (canLogIn) {
+      let userIn = users.find(
+        (u) => u.id === data.user && u.password === data.password
+      )
+      setUserData(userIn)
+      navigate("/main")
+    } else {
+      navigate("/")
+    }
+  }
+
   return (
     <>
       <main className="login-container">
         <aside className="login-aside"></aside>
         <section className="login-form">
           <h1>Login Administrator</h1>
-          <form action="">
+          <form onSubmit={validateUser} ref={FormUser}>
             <label className="login-form--label">
               Usuario
               <div className="login-form--input">
-                <input type="text" placeholder="CC" />
+                <input type="text" placeholder="CC" name="user" />
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="icon icon-tabler icon-tabler-id"
+                  // className="icon icon-tabler icon-tabler-id"
                   viewBox="0 0 24 24"
-                  stroke-width="1"
+                  // strokeWidth="1"
                   fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  // strokeLinecap="round"
+                  // strokeLinejoin="round"
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <rect x="3" y="4" width="18" height="16" rx="3" />
@@ -33,18 +67,23 @@ export function Login() {
             <label className="login-form--label">
               Contraseña
               <div className="login-form--input">
-                <input type="password" placeholder="***************" />
+                <input
+                  type={viewPassword ? "text" : "password"}
+                  placeholder="***************"
+                  name="password"
+                />
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="icon icon-tabler icon-tabler-eye"
+                  // className="icon icon-tabler icon-tabler-eye"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  // strokeWidth="1.5"
                   fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  // strokeLinecap="round"
+                  // strokeLinejoin="round"
+                  onClick={handleVisibilityPassword}
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <line x1="3" y1="3" x2="21" y2="21" />
+                  {!viewPassword && <line x1="3" y1="3" x2="21" y2="21" />}
                   <circle cx="12" cy="12" r="2" />
                   <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" />
                 </svg>
---
+++ src/utils/users.js
@@ -0,0 +1,8 @@
+export const users =[
+  {
+    id: '1034776329',
+    password: 'Makita23',
+    name: 'Andres Laguilavo',
+    rol: 'admin',
+  }
+]
