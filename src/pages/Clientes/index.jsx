ca39b27d9866885ab57fdc7b2eda24fdbb173b9a
Andres Laguilavo <lagui2003@gmail.com>
2023-02-10 09:37:13 -0500

    feat: add the logic for customer search table

--- src/pages/Clientes/index.jsx
+++ src/pages/Clientes/index.jsx
@@ -1,8 +1,11 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 import { Busqueda } from "../../components/Busqueda"
 import { RegistroForm } from "../../components/RegistroForm"
+import { Tabla } from "../../components/Tabla"
 
 export function Clientes() {
+
   const Inputs = [
     { name: "Cedula de ciudadanía" },
     { name: "Nombre" },
@@ -12,10 +15,56 @@ export function Clientes() {
     { name: "Telefono" },
   ]
 
+  const headersTablaClientes = [
+    "ID",
+    "Nombre",
+    "Telefono",
+    "Direccion",
+    "Editar",
+  ]
+
+  const dataTablaClientes = [
+    {
+      ID: 1034776329,
+      Nombre: "Andres Laguilavo",
+      Telefono: 3214613258,
+      Direccion: "Madelena",
+    },
+    {
+      ID: 1234567,
+      Nombre: "Bibis Ruiz",
+      Telefono: 987654321,
+      Direccion: "Suba",
+    },
+  ]
+
+  let dataInArray = dataTablaClientes.map((a) => Object.values(a))
+
+
   return (
     <>
       <RegistroForm countInputs={Inputs} />
       <Busqueda />
+      <Tabla headers={headersTablaClientes}>
+        {/* Lo que hay aqui no se ni como lo hice, no tocar */}
+        {dataInArray.map((dataUser, index) => (
+          <React.Fragment key={index}>
+            {dataUser.map((itemData, index) => (
+              <li key={index}>{itemData}</li>
+            ))}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+              <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+              <line x1="16" y1="5" x2="19" y2="8" />
+            </svg>
+          </React.Fragment>
+        ))}
+      </Tabla>
     </>
   )
 }
