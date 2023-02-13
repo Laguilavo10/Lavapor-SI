775281b87f2938233077b642c7512fc2d5b0b12d
Andres Laguilavo <lagui2003@gmail.com>
2023-02-13 18:49:22 -0500

    refactor : delete customStyles of FormGrid

--- src/components/FormGrid/index.jsx
+++ src/components/FormGrid/index.jsx
@@ -1,16 +1,9 @@
 import { useState } from "react"
 
-export function FormGrid({ gridColumns = 3, children, customStyles, submitFunction, referencia }) {
-  const defaultStyles = {
-    display: "grid",
-    gridTemplateColumns: `repeat( ${gridColumns}, 1fr)`,
-    gap: '20px',
-    margin: '20px 0',
-  }
-  //el form no recibe button debe de ser creado desde el componente padre como children
+export function FormGrid({children, className, submitFunction, referencia }) {
   return (
     <>
-      <form action="" style={customStyles || defaultStyles} onSubmit={submitFunction} ref={referencia || null}>
+      <form action="" className={`form ${className || ''}`} onSubmit={submitFunction} ref={referencia || null}>
         {children}
       </form>
     </>
