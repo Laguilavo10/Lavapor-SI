66111d231b5e8e376dfa9b034145bcc955a0b1db
Andres Laguilavo <lagui2003@gmail.com>
2023-02-16 18:09:50 -0500

    refactor : add svg[search] to the collection

--- src/svg/collectionSVG.jsx
+++ src/svg/collectionSVG.jsx
@@ -110,3 +110,19 @@ SVG["efectivo"] = (
     />
   </svg>
 )
+
+SVG["search"] = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon icon-tabler icon-tabler-search"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <circle cx="10" cy="10" r="7" />
+    <line x1="21" y1="21" x2="15" y2="15" />
+  </svg>
+)
